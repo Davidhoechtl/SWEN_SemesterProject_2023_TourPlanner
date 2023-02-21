e6ed32c9488a3326a140364a53c02e3053f4ee40
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-02-21 15:34:23 +0100

    Added Caliburn startup functionality

---
+++ TourPlannerFrontEnd/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlannerFrontEnd
+{
+    internal class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
