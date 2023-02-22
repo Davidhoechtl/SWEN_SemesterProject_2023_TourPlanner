d81560d983cbe3ffa46008d4c7042e3f5d5a0523
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-02-22 14:11:51 +0100

    Added DataAccess Project

---
+++ TourPlanner.DataAccess/DatabaseIoC.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Npgsql;
+using System.Data;
+
+namespace MTCG.DAL
+{
+    public class DatabaseIoC : Autofac.Module
+    {
+        public DatabaseIoC(ContainerBuilder builder)
+        {
+            this.builder = builder;
+        }
+        
+        public void Load()
+        {
+            IDbConnection dbConnection = new NpgsqlConnection("Host=localhost;Username=postgres;Password=test;Database=TourPlanner");
+            builder.RegisterInstance(dbConnection);
+
+            builder.RegisterTypes(
+               typeof(NpgSqlQueryDatabase)
+            )
+            .SingleInstance()
+            .AsImplementedInterfaces();
+
+            builder.RegisterTypes(
+               typeof(UnitOfWorkFactory)
+            )
+            .SingleInstance();
+        }
+
+        private readonly ContainerBuilder builder;
+    }
+}
---
+++ TourPlanner.DataAccess/NpgSqlQueryDatabase.cs
@@ -0,0 +1,73 @@
+﻿
+namespace MTCG.DAL
+{
+    using Npgsql;
+    using System;
+    using System.Data;
+
+    public class NpgSqlQueryDatabase : IQueryDatabase
+    {
+        public NpgSqlQueryDatabase(IDbConnection dbConnection)
+        {
+            this.dbConnection = new NpgsqlConnection(dbConnection.ConnectionString);
+            this.dbConnection.Open();
+        }
+
+        public T GetItem<T>(string statement, Func<NpgsqlDataReader, T> selector, params NpgsqlParameter[] parameter)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand(statement, dbConnection);
+            foreach(var param in parameter)
+            {
+                cmd.Parameters.Add(param);
+            }
+            cmd.Prepare();
+
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            return selector.Invoke(reader);
+        }
+
+        public List<T> GetItems<T>(string statement, Func<NpgsqlDataReader, List<T>> selector, params NpgsqlParameter[] parameter)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand(statement, dbConnection);
+            foreach (var param in parameter)
+            {
+                cmd.Parameters.Add(param);
+            }
+            cmd.Prepare();
+
+            return selector.Invoke(cmd.ExecuteReader());
+        }
+
+        public int ExecuteNonQuery(string statement, params NpgsqlParameter[] parameter)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand(statement, dbConnection);
+            foreach (var param in parameter)
+            {
+                cmd.Parameters.Add(param);
+            }
+            cmd.Prepare();
+            return cmd.ExecuteNonQuery();
+        }
+
+        public int? InsertAndGetLastIdentity(string statement, params NpgsqlParameter[] parameter)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand(statement, dbConnection);
+            foreach (var param in parameter)
+            {
+                cmd.Parameters.Add(param);
+            }
+            cmd.Prepare();
+
+            object? identity = cmd.ExecuteScalar();
+            if(identity != null)
+            {
+                return (int)identity;
+            }
+
+            return null;
+        }
+
+        private readonly NpgsqlConnection dbConnection;
+    }
+}
