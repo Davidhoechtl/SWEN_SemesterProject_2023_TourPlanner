880a365c93aa7de963cefc14cde33f6debd2f60a
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-02-24 10:14:09 +0100

    Added extension method for npgsqldatareader

---
+++ TourPlanner.DataAccess/Extensions/NpgsqlDataReaderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.DataAccess.Extensions
+{
+    public static class NpgsqlDataReaderExtensions
+    {
+        public static T GetValue<T>(this NpgsqlDataReader reader, string fieldName)
+        {
+            if (reader.IsOnRow)
+            {
+                object value = reader.GetValue(reader.GetOrdinal(fieldName));
+                return (T)value;
+            }
+
+            throw new Exception("Reader is not on a row");
+        }
+    }
+}
