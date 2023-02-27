89eaff2bc98431f208a4c357ba573466c2d190a4
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-02-27 21:35:16 +0100

    Added multi path api key loading

--- TourPlannerBackEnd/Infrastructure/ApiKeyLoader.cs
+++ TourPlannerBackEnd/Infrastructure/ApiKeyLoader.cs
@@ -1,19 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TourPlannerBackEnd.Infrastructure
+﻿namespace TourPlannerBackEnd.Infrastructure
 {
     public class ApiKeyLoader
     {
-        private const string ApiKeyPath = @"C:\Studium\MapQuestApiKey.txt";
+        // enter your api key location here
+        private readonly string[] ApiKeyPaths = new string[] {
+            @"C:\Studium\MapQuestApiKey.txt",
+            @"D:\Studium\Sommersemester 2023\SWEN2\Data\MapQuestApiKey.txt"
+        };
+
         public string Load()
         {
-            using (StreamReader reader = new StreamReader(ApiKeyPath))
+            string key;
+            foreach(string path in ApiKeyPaths)
             {
-                return reader.ReadToEnd();
+                key = TryLoadApiKeyFrom(path);
+                if(string.IsNullOrEmpty(key) == false)
+                {
+                    return key;
+                }
+            }
+
+            throw new Exception("The Api key was not found!");
+        }
+
+        private string TryLoadApiKeyFrom(string path)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                // not found or other exception
+                return null;
             }
         }
     }
