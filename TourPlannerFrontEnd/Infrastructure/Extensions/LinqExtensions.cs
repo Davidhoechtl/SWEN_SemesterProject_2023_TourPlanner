adb10f90acb8d5bf8fbd83db24d293b4b2fb77f5
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-03-02 11:08:53 +0100

    added linq extension for selection of viewmodels

---
+++ TourPlannerFrontEnd/Infrastructure/Extensions/LinqExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlannerFrontEnd.Infrastructure.Extensions
+{
+    internal static class LinqExtensions
+    {
+        public static IEnumerable<T2> SelectViewModels<T, T2>(this IEnumerable<T> models)
+            where T : class
+            where T2 : ViewModel<T>
+        {
+            List<T2> viewModels = new List<T2>();
+            foreach (T model in models)
+            {
+                T2 viewModel = (T2)Activator.CreateInstance(typeof(T2));
+                viewModel.Model = model;
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
+        }
+    }
+}
