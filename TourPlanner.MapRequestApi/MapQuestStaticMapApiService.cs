edf4407c9f8cc32a5e2ebd2f62da38b05706ac12
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-03-14 12:47:22 +0100

    Added Static Map Api Service

---
+++ TourPlanner.MapRequestApi/MapQuestStaticMapApiService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.MapQuestApi
+{
+    internal class MapQuestStaticMapApiService
+    {
+        private const string relativeUrl = "directions/v2/route";
+        private readonly string apiKey;
+        public MapQuestStaticMapApiService(string apiKey)
+        {
+            client = new HttpRestClient();
+            this.apiKey = apiKey;
+        }
+
+        public byte[] GetTourImage(string start, string end)
+        {
+            return null;
+        }
+
+        HttpRestClient client;
+    }
+}
