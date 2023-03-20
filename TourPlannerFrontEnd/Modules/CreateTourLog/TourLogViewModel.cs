8c2449bebb3b1795876642099de615a21c0ff60e
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-03-20 21:07:17 +0100

    implemented TourLogViewModel

--- TourPlannerFrontEnd/Modules/CreateTourLog/TourLogViewModel.cs
+++ TourPlannerFrontEnd/Modules/CreateTourLog/TourLogViewModel.cs
@@ -1,12 +1,60 @@
-﻿using System;
-
-
+﻿
 namespace TourPlannerFrontEnd.Modules.RateTour
 {
+    using System;
     using TourPlanner.DataTransferObjects.Models;
     using TourPlannerFrontEnd.Infrastructure;
 
     internal class TourLogViewModel : ViewModel<TourLog>
     {
+        public DateTime Date
+        {
+            get => this.Model.Date;
+            set
+            {
+                this.Model.Date = value;
+                NotifyOfPropertyChange(nameof(Date));
+            }
+        }
+
+        public string Comment
+        {
+            get => this.Model.Comment;
+            set
+            {
+                this.Model.Comment = value;
+                NotifyOfPropertyChange(nameof(Comment));
+            }
+        }
+
+        public double TakenTimeInMinutes
+        {
+            get => this.Model.TakenTimeInSeconds;
+            set
+            {
+                this.Model.TakenTimeInSeconds = value;
+                NotifyOfPropertyChange(nameof(TakenTimeInMinutes));
+            }
+        }
+
+        public int Difficulty
+        {
+            get => this.Model.Difficulty;
+            set
+            {
+                this.Model.Difficulty = value;
+                NotifyOfPropertyChange(nameof(Difficulty));
+            }
+        }
+
+        public int Rating
+        {
+            get => this.Model.Rating;
+            set
+            {
+                this.Model.Rating = value;
+                NotifyOfPropertyChange(nameof(Rating));
+            }
+        }
     }
 }
